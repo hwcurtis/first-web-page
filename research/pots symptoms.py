2e2274f1ec580dac296d5330293a984831f8d3c6
hwcurtis <you@example.com>
2025-07-16 02:26:15 -0400

    dysautonomia

---
+++ research/pots symptoms.py
@@ -0,0 +1,38 @@
+"""
+POTS has several possible symptoms, and they vary from person to person.
+"""
+
+symptoms = [
+	"Dizziness or lightheadedness, especially when standing up, during prolonged standing in one position or on long walks.",
+	"Fainting or near fainting.",
+	"Forgetfulness and trouble focusing (brain fog).",
+	"Heart palpitations or racing heart rate.",
+	"Exhaustion/fatigue.",
+	"Feeling nervous or anxious.",
+	"Shakiness and excessive sweating.",
+	"Shortness of breath (dyspnea).",
+	"Chest pain.",
+	"Headaches.",
+	"Feeling sick.",
+	"Bloating.",
+	"A pale face and purple discoloration of your hands and feet if they are lower than the level of your heart.",
+	"Disrupted sleep from chest pain, racing heart rate and excessive sweating during sleep."
+]
+
+worsening_situations = [
+	"Being in warm environments, such as in a hot bath or shower or on a hot day.",
+	"Standing frequently, such as when you are waiting in line or shopping.",
+	"Participating in strenuous exercise.",
+	"When you are sick, such as from a cold or an infection.",
+	"Having your period (menstruation)."
+]
+
+"""
+What causes POTS?
+Researchers are not sure yet what exactly causes POTS. Currently, they think there are multiple causes, which they have grouped into different subtypes of POTS, including:
+Neuropathic POTS: This happens when peripheral denervation (loss of nerve supply) leads to poor blood vessel muscles, especially in your legs and core (abdomen).
+Hyperadrenergic POTS: This happens when your sympathetic nervous system is overactive.
+Hypovolemic POTS: Reduced blood volume can lead to POTS. Low blood volume can cause similar symptoms that may overlap in neuropathic and hyperadrenergic POTS.
+There is also growing evidence suggesting that POTS
+"""
+
